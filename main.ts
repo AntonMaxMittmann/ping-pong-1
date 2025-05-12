5948c6815dabbb5296fcb01b496a50331c38cd23
Anton Max Mittmann <antmitt04@gmail.com>
2025-05-12 14:39:53 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+let ball: game.LedSprite = null
+let schlaeger: game.LedSprite = null
+let x_position = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    ball = game.createSprite(receivedNumber, 0)
+    basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        ball.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+    }
+    if (schlaeger.get(LedSpriteProperty.X) == ball.get(LedSpriteProperty.X)) {
+        posTest()
+    } else {
+        basic.showString("lost!")
+        ball.delete()
+        schlaeger.delete()
+    }
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    schlaeger.change(LedSpriteProperty.X, -1)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    schlaeger = game.createSprite(2, 4)
+    radio.setGroup(1)
+    ball = game.createSprite(randint(0, 4), 4)
+    basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        ball.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+    if (ball.get(LedSpriteProperty.Y) == 0) {
+        x_position += ball.get(LedSpriteProperty.X)
+        ball.delete()
+        radio.sendNumber(x_position)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    schlaeger.change(LedSpriteProperty.X, 1)
+})
+function posTest () {
+    if (ball.get(LedSpriteProperty.Y) == 4) {
+        ball.delete()
+        ball = game.createSprite(randint(0, 4), 4)
+        for (let index = 0; index < 4; index++) {
+            ball.change(LedSpriteProperty.Y, -1)
+            basic.pause(500)
+        }
+        radio.sendNumber(ball.get(LedSpriteProperty.X))
+        ball.delete()
+    }
+}
