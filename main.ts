1f2622760cf6bb803b6c22e644d6cd8b1de84135
Anton Max Mittmann <antmitt04@gmail.com>
2025-06-02 14:15:01 +0200

    Bug fixes

--- main.ts
+++ main.ts
@@ -12,8 +12,8 @@ radio.onReceivedNumber(function (receivedNumber) {
         music.playTone(988, music.beat(BeatFraction.Whole))
         posTest()
     } else {
-        basic.showString("You lose!")
         music.playMelody("C5 B A G F E D C ", 391)
+        basic.showString("You lose!")
         ball.delete()
         schlaeger.delete()
     }
@@ -22,19 +22,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     schlaeger.change(LedSpriteProperty.X, -1)
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    schlaeger = game.createSprite(2, 4)
-    radio.setGroup(1)
-    ball = game.createSprite(randint(0, 4), 4)
-    basic.pause(500)
-    for (let index = 0; index < 4; index++) {
-        ball.change(LedSpriteProperty.Y, -1)
-        basic.pause(500)
-    }
-    if (ball.get(LedSpriteProperty.Y) == 0) {
-        x_position += ball.get(LedSpriteProperty.X)
-        ball.delete()
-        radio.sendNumber(x_position)
-    }
+    start()
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     schlaeger.change(LedSpriteProperty.X, 1)
@@ -49,5 +37,20 @@ function posTest () {
         }
         radio.sendNumber(ball.get(LedSpriteProperty.X))
         ball.delete()
+    }
+}
+function start () {
+    schlaeger = game.createSprite(2, 4)
+    radio.setGroup(1)
+    ball = game.createSprite(randint(0, 4), 4)
+    basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        ball.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+    if (ball.get(LedSpriteProperty.Y) == 0) {
+        x_position += ball.get(LedSpriteProperty.X)
+        ball.delete()
+        radio.sendNumber(x_position)
     }
 }
