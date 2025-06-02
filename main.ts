9027cb55d1eaba500dad62d81bd5948b4f954b82
Anton Max Mittmann <antmitt04@gmail.com>
2025-06-02 19:39:19 +0200

    Counter works a bit

--- main.ts
+++ main.ts
@@ -1,40 +1,38 @@
-let ball: game.LedSprite = null
-let schlaeger: game.LedSprite = null
-let x_position = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    ball = game.createSprite(receivedNumber, 0)
-    basic.pause(500)
-    for (let index = 0; index < 4; index++) {
-        ball.change(LedSpriteProperty.Y, 1)
-        basic.pause(500)
-    }
-    if (schlaeger.get(LedSpriteProperty.X) == ball.get(LedSpriteProperty.X)) {
-        music.playTone(988, music.beat(BeatFraction.Whole))
-        posTest()
-    } else {
-        music.playMelody("C5 B A G F E D C ", 391)
-        basic.showIcon(IconNames.Sad, 1000)
+    if (receivedNumber == 10) {
         ball.delete()
         schlaeger.delete()
+        basic.showLeds(`
+            . . . # #
+            . # . . #
+            # # # . #
+            . # . . #
+            . . . . #
+            `)
+        counter += 1
+        start()
+    } else {
+        ball = game.createSprite(receivedNumber, 0)
+        basic.pause(500)
+        for (let index = 0; index < 4; index++) {
+            ball.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
+        }
+        if (schlaeger.get(LedSpriteProperty.X) == ball.get(LedSpriteProperty.X)) {
+            posTest()
+        } else {
+            radio.sendNumber(10)
+            ball.delete()
+            schlaeger.delete()
+            start()
+        }
     }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     schlaeger.change(LedSpriteProperty.X, -1)
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    schlaeger = game.createSprite(2, 4)
-    radio.setGroup(1)
-    ball = game.createSprite(randint(0, 4), 4)
-    basic.pause(500)
-    for (let index = 0; index < 4; index++) {
-        ball.change(LedSpriteProperty.Y, -1)
-        basic.pause(500)
-    }
-    if (ball.get(LedSpriteProperty.Y) == 0) {
-        x_position += ball.get(LedSpriteProperty.X)
-        ball.delete()
-        radio.sendNumber(x_position)
-    }
+    start()
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     schlaeger.change(LedSpriteProperty.X, 1)
@@ -51,3 +49,22 @@ function posTest () {
         ball.delete()
     }
 }
+function start () {
+    schlaeger = game.createSprite(2, 4)
+    radio.setGroup(1)
+    ball = game.createSprite(randint(0, 4), 4)
+    basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        ball.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+    if (ball.get(LedSpriteProperty.Y) == 0) {
+        x_position += ball.get(LedSpriteProperty.X)
+        ball.delete()
+        radio.sendNumber(x_position)
+    }
+}
+let x_position = 0
+let schlaeger: game.LedSprite = null
+let ball: game.LedSprite = null
+let counter = 0
