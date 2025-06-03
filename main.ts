e1c57a9e51002ef9969de77b09ba22fadd6493f0
Anton Max Mittmann <antmitt04@gmail.com>
2025-06-03 15:37:38 +0200

    eyerything works, just if 2 wins not

--- main.ts
+++ main.ts
@@ -13,19 +13,21 @@ radio.onReceivedNumber(function (receivedNumber) {
         if (counter == 3) {
             radio.sendNumber(100)
             basic.showIcon(IconNames.Happy, 1000)
-            basic.clearScreen()
+            ball.delete()
+            schlaeger.delete()
         } else {
             start()
         }
     } else if (receivedNumber == 100) {
         basic.showIcon(IconNames.Sad, 1000)
-        basic.clearScreen()
+        ball.delete()
+        schlaeger.delete()
     } else {
         ball = game.createSprite(receivedNumber, 0)
-        basic.pause(500)
+        basic.pause(speed)
         for (let index = 0; index < 4; index++) {
             ball.change(LedSpriteProperty.Y, 1)
-            basic.pause(500)
+            basic.pause(speed)
         }
         if (schlaeger.get(LedSpriteProperty.X) == ball.get(LedSpriteProperty.X)) {
             posTest()
@@ -52,7 +54,7 @@ function posTest () {
         ball = game.createSprite(randint(0, 4), 4)
         for (let index = 0; index < 4; index++) {
             ball.change(LedSpriteProperty.Y, -1)
-            basic.pause(500)
+            basic.pause(speed)
         }
         radio.sendNumber(ball.get(LedSpriteProperty.X))
         ball.delete()
@@ -61,11 +63,11 @@ function posTest () {
 function start () {
     schlaeger = game.createSprite(2, 4)
     radio.setGroup(1)
-    ball = game.createSprite(randint(0, 4), 4)
-    basic.pause(500)
+    ball = game.createSprite(randint(0, 4), 3)
+    basic.pause(speed)
     for (let index = 0; index < 4; index++) {
         ball.change(LedSpriteProperty.Y, -1)
-        basic.pause(500)
+        basic.pause(speed)
     }
     if (ball.get(LedSpriteProperty.Y) == 0) {
         x_position += ball.get(LedSpriteProperty.X)
@@ -77,4 +79,6 @@ let x_position = 0
 let schlaeger: game.LedSprite = null
 let ball: game.LedSprite = null
 let counter = 0
+let speed = 0
+speed = 200
 counter = 0
